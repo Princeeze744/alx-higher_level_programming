dffb49c135e892e0c3e0e9fe2848f32024d2a260
Princeeze <Trade2uwin@gmail.com>
2023-07-13 03:26:08 +0100

    Added 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module student"""
+
+
+class Student():
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dict representation of the class"""
+        if attrs is not None:
+            return {key: val for key, val in self.__dict__.items()
+                    if attr in attrs}
+        return self.__dict__
